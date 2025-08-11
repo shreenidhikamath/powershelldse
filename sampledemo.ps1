409aa1d53ded709f26286a9c92a0172fb0869dcf
shreenidhikamath <shreenidhikamath22@gmail.com>
2025-08-11 09:55:21 +0530

    Add files via upload

---
+++ sampledemo.ps1
@@ -0,0 +1,9 @@
+$variable1 = 20;
+$variable2 = 30;
+$string1 = "NITTE"
+$sum = $variable1 + $variable2;
+Write-Host (" Sum is $sum");
+$number1 = [int] (Read-Host "Enter a Number 1");
+$number2 = [int] (Read-Host "Enter a Number 2");
+$result = $number1 + $number2;
+Write-Host (" Sum is $result");
